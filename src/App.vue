e4f3908885a7632a42517ed9ae35a2eaee36ea21
michael ginalick <michael.ginalick@contextmediainc.com>
2017-09-21 20:39:55 -0500

    rendering squares and running server

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <SimonSays></SimonSays>
+  </div>
+</template>
+
+<script>
+import SimonSays from './components/SimonSays'
+export default {
+  name: 'app',
+  components: {
+    SimonSays
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
